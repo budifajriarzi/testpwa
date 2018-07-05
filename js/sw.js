5d4c2cfb70cef79743cbceb6f4f2ff5c90b1476e
Paji <paji_starone@yahoo.co.id>
2018-07-06 00:31:41 +0700

    sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,7 @@
 var cacheName = 'hello-world-page';
 var filesToCache = [
-  '/pwa.html',
-  '/css/hello-world.css'
+  'testpwa/pwa.html',
+  'testpwa/css/hello-world.css'
   
 ];
 self.addEventListener('install', function(e) {
